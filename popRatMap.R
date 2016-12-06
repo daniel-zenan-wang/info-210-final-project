fee78c6a3a86c633969864f96ee287bd271f9d27
Karlpa <karlpa@uw.edu>
2016-12-06 14:58:10 -0800

    Final population map

--- popRatMap.R
+++ popRatMap.R
@@ -4,7 +4,7 @@ library(mapdata)
 library(dplyr)
 library(RColorBrewer)
 
-buildCrimeRateMap <- function() {
+
 #This function returns all columns for the county total of a given year
 get.year.total <- function(year) {
   return(filter(df, INDEXYEAR == year) %>% filter(LOCATION == "COUNTY TOTAL"))
@@ -24,13 +24,13 @@ for(i in 1:nrow(totals.2000)) { #Loop through total df
 }
 
 count <- 1 #Declare count integer
-wa_county$total_crimes[1] <- pops[1] #Assign fist county population
+wa_county$population[1] <- pops[1] #Assign fist county population
 for (j in 2:nrow(wa_county)) { #For each row in county dataframe
   temp <- wa_county$subregion[j - 1] #Temp variable of previous county name
   if(wa_county$subregion[j] != temp) { #If the previous name is different than the current
     count <- count + 1 #Increment count
   }
-  wa_county$total_crimes[j] <- pops[count] #Current selected county's rate = crime rate at count
+  wa_county$population[j] <- pops[count] #Current selected county's rate = crime rate at count
 }
 
 
@@ -43,15 +43,15 @@ remove_axes <- theme( ##Remove axes for map, making it look cleaner
   axis.title = element_blank()
 )
 
-#myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))  #USE THIS FOR POPULATION
-#sc <- scale_fill_gradientn(colours = myPalette(100), limits=c(0, 70)) #USE THIS FOR POPULATION
+myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))  #USE THIS FOR POPULATION
+sc <- scale_fill_gradientn(colours = myPalette(100), limits=c(0, 2000000), name = "Population") #USE THIS FOR POPULATION
 
 
 #This draws the map of Washington by county and scales the color to the total crime rate
 wa_base <- ggplot(data = wa_county, mapping = aes(x = long, y = lat, group = group)) +  #Draw map
   coord_fixed(1.3) + #Fix coords of map to look natural
-  geom_polygon(data = wa_county, aes(fill = as.numeric(total_crimes)), color = "white") + #Colorscale
+  geom_polygon(data = wa_county, aes(fill = as.numeric(population)), color = "white") + #Colorscale
   theme_bw() + #Remove gray background
   remove_axes + #Remove axes
-  scale_fill_continuous(name = "Total Crime Rate") #Change name of legend
-  }
+  sc
+  wa_base
